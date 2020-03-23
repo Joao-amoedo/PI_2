90913ce48ec1cd891a3eb3c85dfd32c3bd6c34eb
João Amoêdo <jp.advc@gmail.com>
2020-03-22 23:13:22 -0300

    Adicionado pagamento do PayPal

---
+++ app/src/main/java/com/example/vprojetos/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.vprojetos.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AchIv_gh6GNFVw2bUdSlxFtAPMBfIoW2DOXRinwO4yTa_bSHLH8PRZlguwyOJ2VK2ywNfi1pkeRpPgCD";
+}
