652deb6d62db440cdf4ce2e58625a5a3b0eb05dc
Thiago Santana <thiago.santana.df@hotmail.com>
2020-06-07 16:29:29 -0300

    Revert "Adicionando Comentários"
    This reverts commit 4875161939c8e99bc9c38e22548bd424f427c2b8.

--- app/src/main/java/com/example/vprojetos/config/ConfigPaypal.java
+++
@@ -1,7 +0,0 @@
-package com.example.vprojetos.config;
-
-public class ConfigPaypal {
-
-    public static final String PAYPAL_CLIENT_ID = "AchIv_gh6GNFVw2bUdSlxFtAPMBfIoW2DOXRinwO4yTa_bSHLH8PRZlguwyOJ2VK2ywNfi1pkeRpPgCD";
-
-}
