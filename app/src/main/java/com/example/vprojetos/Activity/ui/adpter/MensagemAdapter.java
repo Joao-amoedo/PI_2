82188ad3c1222d0dcf20d584293bb3093b40b55d
Lucas Andre <lucaslopes05@gmail.com>
2020-06-09 23:26:45 -0300

    [ADD]-crindo chatbot, class MessagemAdapter, ChatActivity

---
+++ app/src/main/java/com/example/vprojetos/Activity/ui/adpter/MensagemAdapter.java
@@ -0,0 +1,56 @@
+package com.example.vprojetos.Activity.ui.adpter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MensagemAdapter {
+
+
+    private List<Mensagem> mensagens;
+    private Activity activity;
+    private int idDoCliente;
+    private View linha;
+    private TextView texto;
+
+    public MensagemAdapter(int idDoCliente, List<Mensagem> mensagens, Activity activity) {
+        this.mensagens = mensagens;
+        this.activity = activity;
+        this.idDoCliente = idDoCliente;
+    }
+
+    @Override
+    public int getCount() {
+        return mensagens.size();
+    }
+
+    @Override
+    public Mensagem getItem(int i) {
+        return mensagens.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        Mensagem mensagem = getItem(i);
+        linha = activity.getLayoutInflater().inflate(R.layout.my_message, viewGroup, false);
+        texto = linha.findViewById(R.id.bot_message);
+
+        if (idDoCliente != mensagem.getVIEW_TYPE()) {
+            linha = activity.getLayoutInflater().inflate(R.layout.bot_message, viewGroup, false);
+            texto = linha.findViewById(R.id.bot_message);
+        }
+        texto.setText(mensagem.getTexto());
+
+        return linha;
+    }
+
+}
