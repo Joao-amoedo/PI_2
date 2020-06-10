7e24ab46a5b26432939414c181871de764f23a42
Thiago Santana <thiago.santana.df@hotmail.com>
2020-06-10 03:19:07 -0300

    Modificação cores do aplicativo

--- app/src/main/java/com/example/vprojetos/Activity/LoginActivity.java
+++ app/src/main/java/com/example/vprojetos/Activity/LoginActivity.java
@@ -63,6 +63,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RecuperarSenhaActivity.class);
                 startActivity(intent);
+
             }
         });
 
